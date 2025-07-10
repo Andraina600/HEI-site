f22610dd32fe34437c8a4af2b55e270eb702c2c7
Roxann <rofaniry@gmail.com>
2025-07-10 09:18:26 +0300

    feat: navbar

---
+++ HEI site/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import logo_HEI from '../../assets/images/472416466_9391920067493318_9000330513189571888_n-removebg-preview.png'
+const p_button = "p-4"
+
+export default function navbar() {
+    return (
+        <>
+            <div className='flex bg-blue-950 gap-80 h-30 items-center'>
+                <div className='pl-15'>
+                    <img className='w-25' src={logo_HEI} alt="logo" />
+                </div>
+                <div className='flex items-center gap-5 pl-10 border-red-500'>
+                    <ul className='flex gap-5 text-white text-base font-bold'>
+                        <li className='hover:text-yellow-500 text-yellow-500 cursor-pointer '><a href=""></a>ACCUEIL</li>
+                        <li className='hover:text-yellow-500 cursor-pointer'><a href=""></a>ACTUALITES</li>
+                        <li className='hover:text-yellow-500 cursor-pointer'><a href=""></a>BOURSE D'ETUDES</li>
+                        <li className='hover:text-yellow-500 cursor-pointer'><a href=""></a>INSCRIPTION</li>
+                    </ul>
+                    <button className= 'text-white bg-yellow-500 cursor-pointer rounded px-4 py-2 font-bold'>INTRANET</button>
+                </div>
+            </div>
+        </>
+    )
+}
