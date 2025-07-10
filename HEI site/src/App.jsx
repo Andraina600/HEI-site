c79941f8db4c33dc1aabca52d5f476e942a2e11e
Andraina <andrainaaxel2504@gmail.com>
2025-07-10 08:08:23 +0300

    feat: add components ok

---
+++ HEI site/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-blue-900 text-3xl p-5'>Vite + React</h1>
+      <br />
+      
+      <p className='text-2xl text-red-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quis culpa cupiditate aut. Impedit mollitia cum tempore asperiores nulla. Praesentium sint id vitae dolor eligendi facilis ducimus animi, velit quam!</p>
+      
+    </>
+  )
+}
+
+export default App
