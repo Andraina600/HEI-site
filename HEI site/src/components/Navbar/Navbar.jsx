6a1dfbcb65627e44abc8ae4d7035418d0ef0e743
Roxann <rofaniry@gmail.com>
2025-07-10 16:50:37 +0300

    feat: Navbar styling touch-ups

--- HEI site/src/components/Navbar/Navbar.jsx
+++ HEI site/src/components/Navbar/Navbar.jsx
@@ -4,18 +4,18 @@ const p_button = "p-4"
 export default function navbar() {
     return (
         <>
-            <div className='flex bg-blue-950 gap-80 h-30 items-center'>
+            <div className='flex bg-blue-950 gap-77 h-27 items-center'>
                 <div className='pl-15'>
-                    <img className='w-25' src={logo_HEI} alt="logo" />
+                    <img className='w-26' src={logo_HEI} alt="logo" />
                 </div>
-                <div className='flex items-center gap-5 pl-10 border-red-500'>
-                    <ul className='flex gap-5 text-white text-base font-bold'>
-                        <li className='hover:text-yellow-500 text-yellow-500 cursor-pointer '><a href=""></a>ACCUEIL</li>
+                <div className='flex items-center gap-6 pl-10 border-red-500'>
+                    <ul className='flex gap-7 text-white text-[1.15rem] font-[650]'>
+                        <li className='text-yellow-500 cursor-pointer '><a href=""></a>ACCUEIL</li>
                         <li className='hover:text-yellow-500 cursor-pointer'><a href=""></a>ACTUALITES</li>
                         <li className='hover:text-yellow-500 cursor-pointer'><a href=""></a>BOURSE D'ETUDES</li>
                         <li className='hover:text-yellow-500 cursor-pointer'><a href=""></a>INSCRIPTION</li>
                     </ul>
-                    <button className= 'text-white bg-yellow-500 cursor-pointer rounded px-4 py-2 font-bold'>INTRANET</button>
+                    <button className= 'text-white bg-yellow-500 cursor-pointer rounded px-4 py-2 font-bold hover:text-black'>INTRANET</button>
                 </div>
             </div>
         </>
